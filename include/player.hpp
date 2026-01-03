50d76539b70cc2f1d04c96292b7f551dfb987817
Renan-G-projec <renanfartes@gmail.com>
2026-01-03 09:32:12 -0300

    Scaffolding Player refactored class

---
+++ include/player.hpp
@@ -0,0 +1,34 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "resourceManager.hpp"
+#include "animation.hpp"
+
+class Player {
+private:
+    // Textures and animations
+    Texture2D sprite;
+    Animation idleAnimation;
+    Animation runLeftAnimation;
+    Animation runRightAnimation;
+
+    // Current position
+    Vector2 position;
+    Vector2 positionInTiles;
+
+    // Velocities and stuff
+    Vector2 velocity;
+
+    // Current pointers
+    Animation* currentAnimation;
+
+public:
+    Player(ResourceManager& resourceManager);
+
+    void jump(float jumpStrength);
+
+    void draw();
+    void update();
+};
+
+#endif
